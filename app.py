2d7b88f6e5ce5bba9c7208848370df3680d1caa3
Lenka <mackova.lenka@protonmail.com>
2023-12-19 12:42:02 +0100

    Initiall commit

---
+++ app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from PIL import Image
+from io import BytesIO
+
+
+st.title('Color to Grayscale Converter')
+
+# Create a file uploader component allowing the user upload a file
+uploaded_image = st.file_uploader('Upload an image')
+
+with st.expander('Start Camera'):
+    # Start the camera
+    camera_image = st.camera_input('Camera')
+
+if camera_image:
+    # Create a pillow image instance
+    img = Image.open(camera_image)
+    # Convert pillow img to gray scale
+    gray_img = img.convert('L')
+    # Render the gray image on the webpage
+    st.image(gray_img)
+
+    buf = BytesIO()
+    gray_img.save(buf, format='JPEG')
+    byte_img = buf.getvalue()
+
+    st.download_button('Download Image',
+                       data=byte_img,
+                       file_name='Download Image')
+
+if uploaded_image:
+    img = Image.open(uploaded_image)
+    gray_img = img.convert('L')
+    st.image(gray_img)
+
+    buf = BytesIO()
+    gray_img.save(buf, format='JPEG')
+    byte_img = buf.getvalue()
+
+    st.download_button('Download Image',
+                       data=byte_img,
+                       file_name='Download Image')
